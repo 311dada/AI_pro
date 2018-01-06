fdcb821f15e0e4e0e1249ca3aff4b619136c46a8
311dada <710485715@qq.com>
2018-01-06 20:31:25 +0800

    new version

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # AI_pro
+
+include two parts:
+
+(1)using svm classifier, PCA to reduce dimension
+
+(2)deep learning
---
+++ SVM.py
@@ -0,0 +1,70 @@
+import numpy as np
+
+
+# x is a 2-dimention list
+#k is the output dimention
+#the output is a low-dimention numpy array
+def PCA(x,k):
+  print ('PCA is starting!')
+  X = np.array(x)
+  X = X - np.mean(X,axis = 1,keepdims = True)
+  m = X.shape[1]
+  C = 1.0/m*np.dot(X,X.T)
+  e,e_vector = np.linalg.eig(C)
+  n = ev.shape[0]
+  ev = ev[np.argsort(-e),:]
+  P = ev[np.arange(k),:]
+  Y = np.dot(P,X)
+  print ('PCA has finished!')
+  return Y
+
+#load the raw data as a matrix
+
+def load_raw_data():
+  f = open('microarray.original.txt','r')
+  content = f.readline()
+  data = [[] for i in range(22282)]
+  k = 0
+  content = f.readline()
+  while True:
+    content = f.readline()
+    if content =='':
+       break
+    content = content.strip().split('\t')
+    del content[0]
+    for i in content:
+      data[k].append(float(i))
+    k = k + 1
+    if k%1000==0:
+       print (str(float(k)/22282)+'%')
+  print ("the data has been loaded completely!")
+  f.close()
+  return data
+
+
+dic_label_num = dict() #dictionary: key is label name, value is label No
+dic_exp_label = dict() #dictionary: key is exp No, value is label name
+
+def init_dic_label():
+    filename = "E-TABM-185.sdrf.txt"
+    lines = open(filename).read().split('\n')
+    label_num = 0
+    y = np.array([0 for i in range(5896)])
+    for i in range(1, len(lines)):
+        segment = lines[i].split('\t')
+        if(len(segment) < 30): continue
+        value = segment[29] #Factor Value [ORGANISMPART]
+        if (value not in dic_label_num):
+            dic_label_num[value] = label_num
+            label_num += 1
+        dic_exp_label[i-1] = value
+        y[i-1] = dic_label_num[value]
+    print ('the labeling has finished!')
+    return y
+
+x = load_raw_data()
+X_raw = PCA(x,20000).T
+init_dic_label()  
+y_raw = init_dic_label()   
+
+
